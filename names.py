ae01e759dcfb5f09b24f2ea69bf56e262ecd4315
moprocha <oprocha@wszib.edu.pl>
2024-12-04 14:09:04 +0100

    Zmodyfikowano kod.

--- names.py
+++ names.py
@@ -1,2 +1,2 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
-# i dodaj jeszcze jakiś ciekawy kod
+print("Mam na imię Maciej")  # tutaj wpisz swoje imię
+print(print("Mam na imię Maciej"))# i dodaj jeszcze jakiś ciekawy kod
